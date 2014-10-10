104ded1a0abdc5c9d9bd1dad654b8b54a38c8b54
Tamas Gal <tgal@pi4062.physik.uni-erlangen.de>
2014-10-10 22:32:43 +0200

    Adds first control host connection trial

--- OnlineReader.cxx
+++ OnlineReader.cxx
@@ -5,14 +5,52 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string>
 
 #include "controlhost/choo.h"
 
+void connect(ControlHost *& ch) {
+    std::cout << "Creating control host client." << std::endl;
+    ControlHost::Throw(true);
+    try {
+        ch = new ControlHost ("131.188.167.62", 5553);
+        ch->MyId("OnlineReader");
+        ch->Subscribe("a MSG");
+        ch->SendMeAlways();
+        std::cout << "Client ready." << std::endl;
+    } catch (ControlHost::Exception error) {
+        std::cout << "Connecting failed." << std::endl;
+        delete ch;
+        ch = NULL;
+    }
+}
+
 int main(int argc, char **argv)
 {
-   std::cout << "test";    
+    ControlHost *ch = NULL;	
+    ControlHost::Throw(true); 
+
+    connect(ch);
+
+    std::cout << "Checking connection." << std::endl;
+    if (ch->Connected()) {
+        std::cout << "Connected to Ligier" << std::endl;
+    } else {
+        std::cout << "Could not connect to Ligier. Exiting..." << std::endl;
+        return -1;
+    }
     
-   ControlHost *ch1 = new ControlHost ("auge", CHOO_WRITE, "Conn 1");
+    std::string tag;
+    int nbytes = 0;
+
+    ch->WaitHead(tag, nbytes);
+    
+    std::cout << "Got a tag " << std::endl;
+    if (tag == "FOO") {
+        std::cout << "Got FOO!" << std::endl;
+    //    ch->GetFullData(buffer, nbytes);
+    }
 }
+
 
 #endif
